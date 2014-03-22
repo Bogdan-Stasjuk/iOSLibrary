f3100f9f58db46e0a30e09f5340b0a74a86247de
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2014-03-23 01:39:59 +0200

    Some changes in .gitignore and UnavailableMethods.h

--- UnavailableMethods.h
+++ UnavailableMethods.h
@@ -5,8 +5,14 @@
 
 #pragma mark - Unavailable methods
 
+#pragma mark -UIViewController
+
 - (id)init __attribute__((unavailable("init not available")));
 - (id)initWithCoder:(NSCoder *)aDecoder __attribute__((unavailable("initWithCoder: not available")));
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil __attribute__((unavailable("initWithNibName:bundle: not available")));
+
+#pragma mark -UIView
+
 - (id)initWithFrame:(CGRect)frame __attribute__((unavailable("initWithFrame: not available")));
 
 @end
