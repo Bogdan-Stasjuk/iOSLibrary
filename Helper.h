be32b10290e8a7a8eaf72283300012deac6f6290
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2014-03-24 17:32:16 +0200

    Added example of setting methods as deprecated

--- Helper.h
+++ Helper.h
@@ -7,3 +7,8 @@
 #pragma clang diagnostic ignored "-Wundeclared-selector"
         [self performSelector:@selector(undeclaredSelector)];
 #pragma clang diagnostic pop
+
+
+#pragma mark - set method as depricated
+
+- (void)someMethod __attribute__((deprecated));
