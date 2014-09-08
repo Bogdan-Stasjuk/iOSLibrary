1a2bcaf6f7c25a943e6015b4d97619b73ac90645
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2014-09-08 12:32:20 +0300

    id replaced by instancetype

--- UnavailableMethods.h
+++ UnavailableMethods.h
@@ -7,26 +7,26 @@
 
 #pragma mark -NSObject
 
-+ (id)new __attribute__((unavailable));
++ (instancetype)new __attribute__((unavailable));
 
 #pragma mark -UIView
 
-- (id)init __attribute__((unavailable));
-- (id)initWithCoder:(NSCoder *)aDecoder __attribute__((unavailable));
-- (id)initWithFrame:(CGRect)frame __attribute__((unavailable));
+- (instancetype)init __attribute__((unavailable));
+- (instancetype)initWithCoder:(NSCoder *)aDecoder __attribute__((unavailable));
+- (instancetype)initWithFrame:(CGRect)frame __attribute__((unavailable));
 
 #pragma mark -UIViewController
 
-- (id)init __attribute__((unavailable));
-- (id)initWithCoder:(NSCoder *)aDecoder __attribute__((unavailable));
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil __attribute__((unavailable));
+- (instancetype)init __attribute__((unavailable));
+- (instancetype)initWithCoder:(NSCoder *)aDecoder __attribute__((unavailable));
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil __attribute__((unavailable));
 
 #pagma mark -UITableViewController
 
-- (id)initWithStyle:(UITableViewStyle)style __attribute__((unavailable));
+- (instancetype)initWithStyle:(UITableViewStyle)style __attribute__((unavailable));
 
 #pragma mark -UIPopoverController
 
-- (id)initWithContentViewController:(UIViewController *)viewController __attribute__((unavailable));
+- (instancetype)initWithContentViewController:(UIViewController *)viewController __attribute__((unavailable));
 
 @end
